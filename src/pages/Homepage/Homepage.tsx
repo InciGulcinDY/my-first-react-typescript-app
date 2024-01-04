63a20452e324df73870e25da053b6490f42b3261
İnci Gülçin Durak Yolcu <113159986+InciGulcinDY@users.noreply.github.com>
2024-01-04 07:08:49 +0300

    init

---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../../components/ProductCard/ProductCard'
+import { ProductModel } from '../../models/responses/ProductModel'
+import ProductService from '../../services/ProductService'
+
+type Props = {}
+
+const Homepage = (props: Props) => {
+
+    const [products,setProducts] = useState<ProductModel[]>([]);
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    const fetchProducts = () => {
+
+        ProductService.getAll().then((response : any) => {
+            setProducts(response.data.products);
+        })
+    
+    }
+    
+
+  return (
+    <div className="container">
+        <div className="row">
+            {products.map(product => (
+                <div key={product.id} className="col-lg-3 col-md-6 col-12 mb-5">
+                    <ProductCard product={product} />
+                </div>
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default Homepage
---
+++ src/services/ProductService.ts
@@ -0,0 +1,19 @@
+import { ProductModel } from '../models/responses/ProductModel';
+import { GetAllProductsModel } from './../models/responses/GetAllProductsModel';
+import axios, { Axios, AxiosResponse } from "axios";
+
+const API_URL = "https://dummyjson.com/product";
+
+class ProductService{
+    getAll() : Promise<AxiosResponse<GetAllProductsModel, any>>
+    {
+        return axios.get<GetAllProductsModel>(API_URL);
+    }
+
+    getById(id: number) 
+    {
+        return axios.get<ProductModel>(API_URL + "/" + id);
+    }
+}
+
+export default new ProductService();
